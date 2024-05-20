08a0a06f20068eda80b088abe90f056b49d2f2db
decodeyourcode <158094606+decodeyourcode@users.noreply.github.com>
2024-05-20 23:08:19 +0100

    Resolve merge conflict

--- database.js
+++ database.js
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
 const { createClient } = require('@supabase/supabase-js');
  
 const supabaseUrl = 'https://vniorzzxrynrhvhffefd.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuaW9yenp4cnlucmh2aGZmZWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MzUxMTAsImV4cCI6MjAzMTIxMTExMH0.PCLDSPZoiTJEOyoutxATAeLWncbOzg0M3-lat9ukzm0'
 
 module.exports = createClient(supabaseUrl, supabaseKey);
-=======
-const { Pool } = require('pg')
- 
-/*
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
-
-pool.connect((err) => {
-    if (err) throw err
-    console.log("Connect to PostgreSQL successfully!")
-})
-
-module.exports = pool*/
->>>>>>> f2e05b7e6de7f9fa5c93dae06857c9c02fa4a9d2
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express")
 const app = express()
 app.use(express.json())
@@ -8,27 +7,3 @@ const userRouter = require('./routes/user.router')
 app.use("/api/v1/user", userRouter)
 
 app.listen(5000, () => console.log("Server is running on port 5000"))
-=======
-/*
-const express = require("express")
-const app = express()
-
-require('dotenv').config()
-
-app.use(express.json())
-
-
-const bookRouter = require('./routes/book.router')
-
-app.use("/api/v1/books", bookRouter)
-
-app.listen(process.env.PORT, () => console.log("Server is running on port 5000"))*/
-const express = require("express");
-const app = express();
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
-app.listen(3000, function () {
-    console.log("Server is running on localhost 3000");
-});
->>>>>>> f2e05b7e6de7f9fa5c93dae06857c9c02fa4a9d2
